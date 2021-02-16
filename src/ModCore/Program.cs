af740c5b7d8f6590e6459185c1294903450a1e5e
Ryan <naamloosdt@gmail.com>
2021-02-16 12:57:35 +0100

    New Project

---
+++ src/ModCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ModCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ModCore NEXT");
+        }
+    }
+}
