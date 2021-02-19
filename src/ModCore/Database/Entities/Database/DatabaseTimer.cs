cea09e8a5ff2e569b17f915fc1e571f1e9d43712
Ryan <naamloosdt@gmail.com>
2021-02-19 16:38:59 +0100

    Some beginnings for Timers

---
+++ src/ModCore/Database/Entities/Database/DatabaseTimer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModCore.Database.Entities.Database
+{
+    public class DatabaseTimer : JsonDatabaseEntity<object>
+    {
+        public ulong UserId;
+        public ulong ChannelId;
+        public ulong Dispatch;
+    }
+}
---
+++ src/ModCore/Services/TimerService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Hosting;
+using ModCore.Bot;
+using ModCore.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ModCore.Services
+{
+    public class TimerService : BackgroundService
+    {
+        private DiscordBot bot;
+        private DatabaseConnectionProvider database;
+
+        public TimerService(DiscordBot bot, DatabaseConnectionProvider database)
+        {
+            this.bot = bot;
+            this.database = database;
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
